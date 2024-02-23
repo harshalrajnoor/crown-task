a972553a0811f0b6c6ecce18ce03332c9baff401
Harshal Rajnoor <harshalrajnor852056@gmail.com>
2024-02-23 19:21:32 +0530

    added internet connection detection

--- lib/login.dart
+++ lib/login.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:crud_app/home_page.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -67,92 +68,129 @@ class _LoginState extends State<Login> {
             style: TextStyle(color: Colors.white),
           ),
         ),
-        body: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 10.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Form(
-                  key: _formKey,
+        body: OfflineBuilder(
+          connectivityBuilder: (
+            BuildContext context,
+            ConnectivityResult connectivity,
+            Widget child,
+          ) {
+            final bool connected = connectivity != ConnectivityResult.none;
+            return Stack(
+              fit: StackFit.expand,
+              children: [
+                Positioned(
+                  height: 24.0,
+                  left: 0.0,
+                  right: 0.0,
+                  child: Container(
+                    color: connected ? Colors.green : Colors.red,
+                    child: Center(
+                      child: Text("${connected ? 'ONLINE' : 'OFFLINE'}"),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 10.0),
                   child: Column(
-                    children: [
-                      // Username text field
-                      TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(
-                          labelText: 'Username',
-                          hintText: 'Enter your Username',
-                          prefixIcon: Icon(
-                            Icons.person,
-                          ),
-                          border: OutlineInputBorder(),
-                        ),
-                        onChanged: (String value) {},
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            userNameTextField = value;
-                            return 'Please enter Username';
-                          }
-                          return null;
-                        },
-                      ),
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              // Username text field
+                              TextFormField(
+                                keyboardType: TextInputType.emailAddress,
+                                decoration: const InputDecoration(
+                                  labelText: 'Username',
+                                  hintText: 'Enter your Username',
+                                  prefixIcon: Icon(
+                                    Icons.person,
+                                  ),
+                                  border: OutlineInputBorder(),
+                                ),
+                                onChanged: (String value) {},
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    userNameTextField = value;
+                                    return 'Please enter Username';
+                                  }
+                                  return null;
+                                },
+                              ),
 
-                      const SizedBox(
-                        height: 10.0,
-                      ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
 
-                      // Password Text Field
-                      TextFormField(
-                        keyboardType: TextInputType.visiblePassword,
-                        decoration: const InputDecoration(
-                          labelText: 'Password',
-                          hintText: 'Enter your Password',
-                          prefixIcon: Icon(
-                            Icons.lock,
-                          ),
-                          border: OutlineInputBorder(),
-                        ),
-                        onChanged: (String value) {},
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            passwordTextField = value;
-                            return 'Please enter Password';
-                          }
-                          return null;
-                        },
-                      ),
-                      MaterialButton(
-                        height: 40,
-                        minWidth: double.infinity,
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            // Login Post request
-                            loginCall();
+                              // Password Text Field
+                              TextFormField(
+                                keyboardType: TextInputType.visiblePassword,
+                                decoration: const InputDecoration(
+                                  labelText: 'Password',
+                                  hintText: 'Enter your Password',
+                                  prefixIcon: Icon(
+                                    Icons.lock,
+                                  ),
+                                  border: OutlineInputBorder(),
+                                ),
+                                onChanged: (String value) {},
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    passwordTextField = value;
+                                    return 'Please enter Password';
+                                  }
+                                  return null;
+                                },
+                              ),
+                              MaterialButton(
+                                height: 40,
+                                minWidth: double.infinity,
+                                onPressed: () async {
+                                  if (_formKey.currentState!.validate()) {
+                                    // Login Post request
+                                    loginCall();
 
-                            // Navigation to HomePage
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const MaterialApp(
-                                  home: HomePage(),
+                                    // Navigation to HomePage
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => const MaterialApp(
+                                          home: HomePage(),
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                },
+                                color: Colors.blue,
+                                child: const Text(
+                                  'Login',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 15),
                                 ),
                               ),
-                            );
-                          }
-                        },
-                        color: Colors.blue,
-                        child: const Text(
-                          'Login',
-                          style: TextStyle(color: Colors.white, fontSize: 15),
+                            ],
+                          ),
                         ),
-                      ),
+                      )
                     ],
                   ),
                 ),
-              )
+              ],
+            );
+          },
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'There are no bottons to push :)',
+              ),
+              Text(
+                'Just turn off your internet.',
+              ),
             ],
           ),
         ),
